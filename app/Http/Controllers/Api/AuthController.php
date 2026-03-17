d2be8995aff40993f6c956b7265b37237631dad1
abdelhakim allouani <abdelhakimallouani@gmail.com>
2026-03-17 11:22:22 +0000

    feat: add API routes for authentication and user management

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+     public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]); 
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['success' => true, 'data' => ['user'=>$user,'access_token' => $token], 'message' => 'User registered successfully']);
+
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['success' => false,
+                'message' => 'Invalid login credentials'
+            ], 401);
+        }
+
+        $user = User::user();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'data'=>['user' => $user, 'access_token' => $token],
+            'message' => 'User logged in successfully',
+        ]);
+    }
+
+   
+
+    public function logout(Request $request)
+    {
+        // Auth::user()->currentAccessToken()->delete();
+
+        return response()->json(['success' => true,'data' => null, 'message' => 'User logged out ']);
+
+    }
+
+    public function me(){
+        return response()->json(['success' => true, 'data' => Auth::user(),'message' => 'User logged in successfully']);
+    }
+}
